d7e35dcd1c362e83d2090edccd03770d7e641003
denver <denver@semiotic.ai>
2025-02-18 16:58:29 -0600

    feat(graphdoc): add check_file_path as helper function

--- graphdoc/graphdoc/data/helper.py
+++ graphdoc/graphdoc/data/helper.py
@@ -26,3 +26,20 @@ def check_directory_path(directory_path: Union[str, Path]) -> None:
         raise ValueError(
             f"The provided path does not resolve to a valid directory: {directory_path}"
         )
+
+
+def check_file_path(file_path: Union[str, Path]) -> None:
+    """
+    Check if the provided path resolves to a valid file.
+
+    :param file_path: The path to check.
+    :type file_path: Union[str, Path]
+    :raises ValueError: If the path does not resolve to a valid file.
+    :return: None
+    :rtype: None
+    """
+    _file_path = Path(file_path).resolve()
+    if not _file_path.is_file():
+        raise ValueError(
+            f"The provided path does not resolve to a valid file: {file_path}"
+        )
