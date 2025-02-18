eb89143dbb5d660fd197cc3a94081877f0b178cc
denver <denver@semiotic.ai>
2025-02-18 15:34:30 -0600

    build(mlflow-manager): add build scripts and makefile for mlflow resources

--- mlflow-manager/run.sh
+++ mlflow-manager/run.sh
@@ -23,6 +23,24 @@ lint_command() {
     poetry run pyright .
 }
 
+# make command
+make_install_command() {
+    make install
+}
+
+make_test_command() {
+    make test
+}
+
+make_clean_command() {
+    make clean
+}
+
+# runs install, test, and clean
+make_all_command() {
+    make all
+}
+
 # help menu
 show_help() {
     echo "Usage: ./nli [option]"
@@ -35,6 +53,12 @@ show_help() {
     echo "  dev                    Install dependencies with dev"
     echo "  format                 Format the code"
     echo "  lint                   Lint the code"
+
+    # make commands
+    echo "  make-install           Install dependencies"
+    echo "  make-test              Run tests"
+    echo "  make-clean             Clean the environment"
+    echo "  make-all               Install, test, and clean"
 }
 
 # handle command line arguments
@@ -50,6 +74,12 @@ else
         "dev") dev_command ;;
         "format") format_command ;;
         "lint") lint_command ;;
+
+        # make commands
+        "make-install") make_install_command ;;
+        "make-test") make_test_command ;;
+        "make-clean") make_clean_command ;;
+        "make-all") make_all_command ;;
         *) show_help ;;
     esac
 fi
