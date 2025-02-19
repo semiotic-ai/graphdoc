0c9432c349bf5eb7c9ba078ec11d853a962d22f7
denver <denver@semiotic.ai>
2025-02-19 14:33:40 -0600

    test(graphdoc): add test__check_node_type to TestParser

--- graphdoc/tests/data/test_parser.py
+++ graphdoc/tests/data/test_parser.py
@@ -6,7 +6,12 @@ from pathlib import Path
 from graphdoc import Parser
 
 # external packages
-from graphql import DocumentNode
+from graphql import (
+    DocumentNode,
+    ObjectTypeDefinitionNode,
+    EnumTypeDefinitionNode,
+    EnumValueDefinitionNode,
+)
 import pytest
 
 # logging
@@ -17,6 +22,51 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 class TestParser:
+
+    def test__check_node_type(self, par: Parser):
+
+        # DEFAULT_NODE_TYPES = {
+        #     DocumentNode: "full schema",
+        #     ObjectTypeDefinitionNode: "table schema",
+        #     EnumTypeDefinitionNode: "enum schema",
+        #     EnumValueDefinitionNode: "enum value",
+        # }
+
+        ALTERNATIVE_NODE_TYPES = {
+            DocumentNode: "test full schema",
+            ObjectTypeDefinitionNode: "test table schema",
+            EnumTypeDefinitionNode: "test enum schema",
+            EnumValueDefinitionNode: "test enum value",
+        }
+        document_node = DocumentNode()
+        object_type_definition_node = ObjectTypeDefinitionNode()
+        enum_type_definition_node = EnumTypeDefinitionNode()
+        enum_value_definition_node = EnumValueDefinitionNode()
+
+        # test the default mapping
+        assert par._check_node_type(document_node) == "full schema"
+        assert par._check_node_type(object_type_definition_node) == "table schema"
+        assert par._check_node_type(enum_type_definition_node) == "enum schema"
+        assert par._check_node_type(enum_value_definition_node) == "enum value"
+
+        # check that we can pass in a custom mapping
+        assert (
+            par._check_node_type(document_node, ALTERNATIVE_NODE_TYPES)
+            == "test full schema"
+        )
+        assert (
+            par._check_node_type(object_type_definition_node, ALTERNATIVE_NODE_TYPES)
+            == "test table schema"
+        )
+        assert (
+            par._check_node_type(enum_type_definition_node, ALTERNATIVE_NODE_TYPES)
+            == "test enum schema"
+        )
+        assert (
+            par._check_node_type(enum_value_definition_node, ALTERNATIVE_NODE_TYPES)
+            == "test enum value"
+        )
+
     def test_parse_schema_from_file(self, par: Parser):
         schema_file = (
             Path(BASE_DIR)
@@ -27,3 +77,18 @@ class TestParser:
         )
         schema = par.parse_schema_from_file(schema_file)
         assert isinstance(schema, DocumentNode)
+
+    # def test_update_node_descriptions(self, par: Parser):
+    #     pass
+
+    # def test_count_description_pattern_matching(self, par: Parser):
+    #     pass
+
+    # def test_fill_empty_descriptions(self, par: Parser):
+    #     pass
+
+    # def test_schema_equality_check(self, par: Parser):
+    #     pass
+
+    # def test_parse_objects_from_full_schema_object(self, par: Parser):
+    #     pass
