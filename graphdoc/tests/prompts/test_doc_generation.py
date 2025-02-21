e444ec5f3b5adf96714f88b22c73c48d9012b0a4
denver <denver@semiotic.ai>
2025-02-20 19:55:47 -0600

    test: add test_doc_generator_prompt to TestDocGeneratorPrompt

--- graphdoc/tests/prompts/test_doc_generation.py
+++ graphdoc/tests/prompts/test_doc_generation.py
@@ -2,8 +2,33 @@
 import logging
 
 # internal packages
+from graphdoc import GraphDoc
+from graphdoc import DocGeneratorPrompt, DocQualityPrompt
+from graphdoc import DocGeneratorSignature
 
 # external packages
+import dspy
+from typing import Callable
 
 # logging
 log = logging.getLogger(__name__)
+
+
+class TestDocGeneratorPrompt:
+    def test_doc_generator_prompt(self, gd: GraphDoc):
+        dgp = DocGeneratorPrompt(
+            prompt="base_doc_gen",
+            prompt_type="chain_of_thought",
+            prompt_metric=DocQualityPrompt(
+                prompt="doc_quality",
+                prompt_type="predict",
+                prompt_metric="rating",
+            ),
+        )
+        assert isinstance(dgp, DocGeneratorPrompt)
+        # assert isinstance(dgp.prompt, DocGeneratorSignature)
+        assert dgp.prompt_type == "chain_of_thought"
+        assert isinstance(dgp.prompt_metric, DocQualityPrompt)
+
+    def test_evaluate_documentation_quality(self):
+        pass
