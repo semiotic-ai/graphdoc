4ee37aab24a2c5d72c896d1093749a7383b362c7
denver <denver@semiotic.ai>
2025-02-24 14:33:50 -0600

    test(data): update TestMlflowDataHelper for full model loading coverage of MlflowDataHelper

--- graphdoc/tests/data/mlflow_data/test_mlflow_data_helper.py
+++ graphdoc/tests/data/mlflow_data/test_mlflow_data_helper.py
@@ -6,22 +6,49 @@ from pathlib import Path
 from graphdoc import MlflowDataHelper
 
 # external packages
+import dspy
 import mlflow
 
 # logging
 log = logging.getLogger(__name__)
 
 # Define the base directory (project root)
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-MLFLOW_DIR = Path(BASE_DIR) / "assets" / "mlruns"
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+MLFLOW_DIR = Path(BASE_DIR) / "tests" / "assets" / "mlruns"
 
 
 class TestMlflowDataHelper:
     def test_init_mlflow_data_helper(self):
-        mlflow_data_helper = MlflowDataHelper(mlflow_tracking_uri=MLFLOW_DIR)
-        assert mlflow_data_helper is not None
-        assert isinstance(mlflow_data_helper, MlflowDataHelper)
-        assert isinstance(mlflow_data_helper.mlflow_client, mlflow.MlflowClient)
+        mdh = MlflowDataHelper(mlflow_tracking_uri=MLFLOW_DIR)
+        assert mdh is not None
+        assert isinstance(mdh, MlflowDataHelper)
+        assert isinstance(mdh.mlflow_client, mlflow.MlflowClient)
+
+    def test_latest_model_version(self):
+        mdh = MlflowDataHelper(mlflow_tracking_uri=MLFLOW_DIR)
+        log.info(f"mlflow_tracking_uri: {mdh.mlflow_tracking_uri}")
+        model = mdh.latest_model_version(model_name="doc_generator_model")
+        assert model is not None
+        assert isinstance(model, dspy.ChainOfThought)
 
     def test_model_by_name_and_version(self):
-        pass
+        mdh = MlflowDataHelper(mlflow_tracking_uri=MLFLOW_DIR)
+        tracking_uri = mlflow.get_tracking_uri()
+        model = mdh.model_by_name_and_version(
+            model_name="doc_generator_model", model_version="1"
+        )
+        assert model is not None
+        assert isinstance(model, dspy.ChainOfThought)
+
+    def test_model_by_uri(self):
+        model_uri = (
+            Path(MLFLOW_DIR)
+            / "513408250948216117"
+            / "976d330558344c41b30bd1531571de18"
+            / "artifacts"
+            / "model"
+        )
+        mdh = MlflowDataHelper(mlflow_tracking_uri=MLFLOW_DIR)
+        model = mdh.model_by_uri(model_uri=str(model_uri))
+        assert model is not None
+        assert isinstance(model, dspy.ChainOfThought)
