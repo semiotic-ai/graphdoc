ce165b60a4e5da4024b85f7378b2280f098d0dcc
denver <denver@semiotic.ai>
2025-02-24 17:07:22 -0600

    test(data): add test_formatted_signature to TestDspyDataHelper

--- graphdoc/tests/data/dspy_data/test_dspy_data_helper.py
+++ graphdoc/tests/data/dspy_data/test_dspy_data_helper.py
@@ -16,7 +16,7 @@ class TestDspyDataHelper:
         class TestSignature(dspy.Signature):
             """Test signature."""
 
-            test_inpu: str = dspy.InputField()
+            test_input: str = dspy.InputField()
             test_output: str = dspy.InputField()
 
         cot = dspy.ChainOfThought(TestSignature)
@@ -30,3 +30,26 @@ class TestDspyDataHelper:
 
         assert isinstance(cot_signature, (dspy.Signature, dspy.SignatureMeta))
         assert isinstance(pred_signature, (dspy.Signature, dspy.SignatureMeta))
+
+    def test_formatted_signature(self):
+        class TestSignature(dspy.Signature):
+            """Test signature."""
+
+            test_input: str = dspy.InputField()
+            test_output: str = dspy.InputField()
+
+        example = dspy.Example(
+            test_input="test input",
+            test_output="test output",
+        )
+
+        formatted_signature = DspyDataHelper.formatted_signature(TestSignature, example)
+
+        assert formatted_signature is not None
+        assert isinstance(formatted_signature, str)
+        assert "------\nSystem\n------" in formatted_signature
+        assert "------\nUser\n------" in formatted_signature
+        assert (
+            formatted_signature
+            == "------\nSystem\n------\n Your input fields are:\n1. `test_input` (str)\n2. `test_output` (str)\n\nYour output fields are:\n\n\nAll interactions will be structured in the following way, with the appropriate values filled in.\n\n[[ ## test_input ## ]]\n{test_input}\n\n[[ ## test_output ## ]]\n{test_output}\n\n\n\n[[ ## completed ## ]]\n\nIn adhering to this structure, your objective is: \n        Test signature. \n------\nUser\n------\n [[ ## test_input ## ]]\ntest input\n\n[[ ## test_output ## ]]\ntest output"
+        )
