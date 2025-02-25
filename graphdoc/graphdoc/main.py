2caf87837415030aa492f63d4e600cfbb51225fe
denver <denver@semiotic.ai>
2025-02-24 18:32:33 -0600

    feat: add single_prompt_from_dict and single_prompt_from_yaml

--- graphdoc/graphdoc/main.py
+++ graphdoc/graphdoc/main.py
@@ -1,9 +1,11 @@
 # system packages
 import logging
-from typing import Literal, Optional
+from pathlib import Path
+from typing import Literal, Optional, Union
 
 # internal packages
-from .data import setup_logging
+from .prompts import SinglePrompt, PromptFactory
+from .data import setup_logging, load_yaml_config
 
 # external packages
 import dspy
@@ -37,3 +39,84 @@ class GraphDoc:
         except Exception as e:
             log.error(f"Error initializing LM: {e}")
             raise e
+
+    #######################
+    # Prompt Methods      #
+    #######################
+    def single_prompt_from_dict(
+        self, prompt_dict: dict, prompt_metric: Union[str, SinglePrompt]
+    ) -> SinglePrompt:
+        """
+        Load a single prompt from a dictionary of parameters.
+
+        {
+            "prompt": "doc_quality",             # Which prompt signature to use
+            "class": "SchemaDocQualityPrompt",   # Must be a child of SinglePrompt (we will use an enum to map this)
+            "type": "predict",                   # The type of prompt to use (predict, chain_of_thought)
+            "metric": "rating",                  # The type of metric to use (rating, category)
+            "load_from_mlflow": false,           # Whether to load the prompt from an MLFlow URI
+            "model_uri": null,                   # The tracking URI for MLflow
+            "model_name": null,                  # The name of the model in MLflow
+            "model_version": null                # The version of the model in MLflow
+            "prompt_metric": False               # Whether another prompt is used to calculate the metric (in which case we must also load that prompt)
+        }
+
+        :param prompt_dict: Dictionary containing prompt information.
+        :type prompt_dict: dict
+        :param prompt_metric: The metric to use to calculate the metric. Can be another prompt signature or a string.
+        :type prompt_metric: Union[str, SinglePrompt]
+        :return: A SinglePrompt object.
+        :rtype: SinglePrompt
+        """
+        try:
+            return PromptFactory.single_prompt(
+                prompt=prompt_dict["prompt"],
+                prompt_class=prompt_dict["class"],
+                prompt_type=prompt_dict["type"],
+                prompt_metric=prompt_metric,
+            )
+        except Exception as e:
+            log.error(f"Error creating single prompt: {e}")
+            raise e
+
+    def single_prompt_from_yaml(self, yaml_path: Union[str, Path]) -> SinglePrompt:
+        """
+        Load a single prompt from a YAML file.
+
+        prompt:
+            prompt: doc_quality                                   # Which prompt signature to use
+            class: SchemaDocQualityPrompt                         # Must be a child of SinglePrompt (we will use an enum to map this)
+            type: predict                                         # The type of prompt to use (predict, chain_of_thought)
+            metric: rating                                        # The type of metric to use (rating, category)
+            load_from_mlflow: false                               # Whether to load the prompt from an MLFlow URI
+            model_uri: null                                       # The tracking URI for MLflow
+            model_name: null                                      # The name of the model in MLflow
+            model_version: null                                   # The version of the model in MLflow
+            prompt_metric: False                                  # Whether another prompt is used to calculate the metric (in which case we must also load that prompt)
+
+        prompt_metric:                                            # Follows the same format as the prompt section
+            prompt: null                                          # The prompt to use to calculate the metric
+            class: null                                           # The class of the prompt to use to calculate the metric
+            type: null                                            # The type of prompt to use to calculate the metric
+            metric: null                                          # The metric to use to calculate the metric
+            load_from_mlflow: false                               # Whether to load the prompt from an MLFlow URI
+            model_uri: null                                       # The tracking URI for MLflow
+            model_name: null                                      # The name of the model in MLflow
+            model_version: null                                   # The version of the model in MLflow
+
+        :param yaml_path: Path to the YAML file.
+        :type yaml_path: str
+        :return: A SinglePrompt object.
+        :rtype: SinglePrompt
+        """
+        config = load_yaml_config(yaml_path)
+        if config["prompt"]["prompt_metric"]:
+            prompt_metric_config = config["prompt_metric"]
+            prompt_metric_metric = prompt_metric_config["metric"]
+            prompt_metric = self.single_prompt_from_dict(
+                prompt_metric_config, prompt_metric_metric
+            )
+        else:
+            prompt_metric = config["prompt"]["metric"]
+        prompt = self.single_prompt_from_dict(config["prompt"], prompt_metric)
+        return prompt
