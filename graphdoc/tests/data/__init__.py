f9b10f3e7ec526697ea10b3c7d8c6d2598da4830
denver <denver@semiotic.ai>
2025-03-06 11:15:16 -0600

    refactor: remove unused tests __init__ files and modify root

--- graphdoc/tests/data/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2025-, Semiotic AI, Inc.
-# SPDX-License-Identifier: Apache-2.0
-
-from .dspy_data import *
-from .test_helper import *
-from .test_local import *
-from .test_parser import *
-from .test_schema import *
